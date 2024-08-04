e80f3ad7d3ddcef5f1a7d92aafc98189a77ce6fe
Arjun278 <145422444+Arjun278@users.noreply.github.com>
2024-08-04 23:30:07 +0930

    Update flask_app.py

--- secure_chat/server/flask_app.py
+++ secure_chat/server/flask_app.py
@@ -3,10 +3,13 @@ from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
 import logging
+import os
 
 app = Flask(__name__)
+
+# Use environment variables for secret keys
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
-app.config['JWT_SECRET_KEY'] = 'your_jwt_secret_key'  # Change this to a random secret key
+app.config['JWT_SECRET_KEY'] = os.getenv('JWT_SECRET_KEY', 'default_jwt_secret_key') 
 db = SQLAlchemy(app)
 jwt = JWTManager(app)
 
@@ -17,7 +20,8 @@ class User(db.Model):
     username = db.Column(db.String(80), unique=True, nullable=False)
     password = db.Column(db.String(120), nullable=False)
 
-with app.app_context():
+@app.before_first_request
+def create_tables():
     db.create_all()
 
 @app.route('/register', methods=['POST'])
@@ -27,10 +31,13 @@ def register():
         username = data.get('username')
         password = data.get('password')
 
+        if not username or not password:
+            return jsonify({"msg": "Username and password are required"}), 400
+
         if User.query.filter_by(username=username).first():
             return jsonify({"msg": "Username already exists"}), 400
 
-        hashed_password = generate_password_hash(password)
+        hashed_password = generate_password_hash(password, method='pbkdf2:sha256', salt_length=16)
         new_user = User(username=username, password=hashed_password)
         db.session.add(new_user)
         db.session.commit()
@@ -46,6 +53,9 @@ def login():
         data = request.get_json()
         username = data.get('username')
         password = data.get('password')
+
+        if not username or not password:
+            return jsonify({"msg": "Username and password are required"}), 400
 
         user = User.query.filter_by(username=username).first()
         if not user or not check_password_hash(user.password, password):
